50bb766a99ee5214987b11b3b23f1bd28effcf1e
Bumhee Han <kjhhjk1234@naver.com>
2019-02-12 19:23:45 +0900

    5.1.3

--- src/main/java/com/bumbing/service/MemberServiceImpl.java
+++ src/main/java/com/bumbing/service/MemberServiceImpl.java
@@ -1,9 +1,12 @@
 package com.bumbing.service;
 
+import java.util.Properties;
+
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -30,6 +33,10 @@ public class MemberServiceImpl implements MemberService {
 	public int signUp(MemberVO mem) {
 		mem.setPwd(pwencoder.encode(mem.getPwd()));
 		int state = memberMapper.signUp(mem);
+		
+
+	
+
 		/*
 		
 		if(state ==1) {
